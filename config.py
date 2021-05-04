e053ab4d9429ad263e295c9e9a7660bde28d4475
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-05-04 23:10:36 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,17 @@
+# import PySimpleGUI as sg
+
+# sg.theme('DarkAmber')
+
+# layout = [[sg.Text('ttool')],
+#                 [sg.Text('API Key', size =(35,1),)],    
+#                 [sg.InputText(key='apikey')],
+#                 [sg.Text('API Secret Key', size = (35, 1))], 
+#                 [sg.InputText(key='apisecret')],
+#                 [sg.Submit(), sg.Cancel()]]
+
+# window = sg.Window('ttool - API input', layout=layout, margins=(100, 50), resizable=True)
+
+# event, values = window.read()
+
+# API_KEY = values['apikey']
+# API_SECRET = values['apisecret']
---
+++ readme.md
@@ -0,0 +1,17 @@
+# ttool.
+## A lightweight tradingtool for binance
+
+  
+
+## Downloading ttool.
+
+ you can download the app for mac on my site: [dowload here](|https://linktoyoursite.com)
+ Windows;
+ Mac:
+ unzip the dowloaded file and your done! :)
+ clone the github reposetory and run ttool.py
+
+## How to use ttool.
+ first you need to setup your api keys, 
+ 
+
