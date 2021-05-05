ff443af7b0dc387dd0509a29b8daac44dce5e937
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-05-05 10:05:42 +0200

    program is not working, but we are a step further

--- ttool.py
+++ ttool.py
@@ -3,9 +3,23 @@ import PySimpleGUI as sg
 
 
 #GUI
+sg.theme('DarkAmber')    #Theme 
+##BUY TAB
+layoutBuy = [[sg.Text('Ticker:', size=(10,1)),sg.Input('',key='ticker')],
+            [sg.Text('Price', size=(10,1)),sg.Input('',key='price')],
+            [sg.Text('Amount', size=(10,1)),sg.Input('',key='quantity')],
+            [sg.Text('percentage amount', size=(10,1)),sg.Slider(0,100, key='percentage Amount')],
+            [sg.Button('buy.')]]
 
+window = sg.Window("ttool", layoutBuy)
+event, values = window.read() 
 
+window.close()
 
+#store the inputs
+price = float(values['price'])
+
+##############################################
 
 #ORDERS
 ##MAKE CONNENCTION WITH BINANCE
