4b27b5b44562bc2364795a583f0aee776ae91523
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-05-06 11:30:08 +0200

    made it work a little bit, still need to create sell tab and make that you dont need to press the button twice

--- ttool.py
+++ ttool.py
@@ -10,51 +10,106 @@ client = Client(config.API_KEY, config.API_SECRET,)
 
 #GUI
 sg.theme('LightBrown12')    #Theme 
+
 ##BUY TAB
-layoutBuy =[[sg.Text('Ticker:',     size=(10,1)),       sg.Input('',size=(15,2), key='buyticker')],
-            [sg.Text('Price',       size=(10,1)),       sg.Input('',size=(15,2), key='buyprice')],
-            [sg.Text('Amount',      size=(10,1)),       sg.Input('',size=(15,2), key='buyquantity')],
+buyordertypechoices = ('Limit', 'Market', 'Stop loss') 
+layoutBuy =[[sg.Text('Ticker:',         size=(10,1)),       sg.Input('',size=(15,2), key='buyticker')],
+            [sg.Text('Order type',      size=(10,1)),       sg.Listbox(buyordertypechoices, size=(10, len(buyordertypechoices)), key='buyordertype')],
+            [sg.Text('Price',           size=(10,1)),       sg.Input('',size=(15,2), key='buyprice')],
+            [sg.Text('Amount',          size=(10,1)),       sg.Input('',size=(15,2), key='buyquantity')],
             [sg.Button('buy.')]]
+##SELL TAB
+sellordertypechoices = ('Limit', 'Market', 'Stop loss') 
+layoutSell =[[sg.Text('Ticker:',         size=(10,1)),       sg.Input('',size=(15,2), key='sellticker')],
+            [sg.Text('Order type',      size=(10,1)),       sg.Listbox(buyordertypechoices, size=(10, len(buyordertypechoices)), key='sellordertype')],
+            [sg.Text('Price',           size=(10,1)),       sg.Input('',size=(15,2), key='sellprice')],
+            [sg.Text('Amount',          size=(10,1)),       sg.Input('',size=(15,2), key='sellquantity')],
+            [sg.Button('sell.')]]
+
+
 
 window = sg.Window("ttool", layoutBuy,)
 
 ##Variables
 #Buy side
 event, value = window.read()
-buyticker = value['buyticker'].rstrip()
-buypricewithoutfloat = value['buyprice'].rstrip()
-buyamountwithoutfloat = value['buyquantity'].rstrip()
-buyprice = float(buypricewithoutfloat)
-buyamount = float(buyamountwithoutfloat)
+####
+try:
+    buyticker = value['buyticker'].rstrip()
+    listbuyordertype = value['buyordertype']
+    buyordertype = listbuyordertype[0]
+    if buyordertype == 'Limit':
+        newbuyordertype = buyordertype.replace('Limit', ORDER_TYPE_LIMIT)
+    if buyordertype == 'Market':
+        newbuyordertype = buyordertype.replace('Market', ORDER_TYPE_MARKET)
+    if buyordertype == 'Stop loss':
+        newbuyordertype = buyordertype.replace('Stop loss', ORDER_TYPE_STOP_LOSS)
+    buypricewithoutfloat = value['buyprice'].rstrip()
+    buyamountwithoutfloat = value['buyquantity'].rstrip()
+    buyprice = float(buypricewithoutfloat)
+    buyamount = float(buyamountwithoutfloat)
+except KeyError:
+    pass
 #Sell side
-
+try:
+    event, value = window.read()
+    sellticker = value['sellticker'].rstrip()
+    listbuyordertype = value['sellordertype']
+    sellordertype = listbuyordertype[0]
+    if sellordertype == 'Limit':
+        newsellordertype = sellordertype.replace('Limit', ORDER_TYPE_LIMIT)
+    if sellordertype == 'Market':
+        newsellordertype = sellordertype.replace('Market', ORDER_TYPE_MARKET)
+    if sellordertype == 'Stop loss':
+        newsellordertype = sellordertype.replace('Stop loss', ORDER_TYPE_STOP_LOSS)
+    sellpricewithoutfloat = value['sellprice'].rstrip()
+    sellamountwithoutfloat = value['sellquantity'].rstrip()
+    sellprice = float(sellpricewithoutfloat)
+    sellamount = float(sellamountwithoutfloat)
+except KeyError:
+    pass
 
 ##Orders
 #Buy orders
-limitbuyOrder = client.create_order(
-    symbol='BNBBTC',
-    side=SIDE_BUY,
-    type=ORDER_TYPE_LIMIT,
-    timeInForce=TIME_IN_FORCE_GTC,
-    quantity=buyamount,
-    price=buyprice)
 
+try:
+    buyOrder = client.create_order(
+        symbol=buyticker,
+        side=SIDE_BUY,
+        type=newbuyordertype,
+        timeInForce=TIME_IN_FORCE_GTC,
+        quantity=buyamount,
+        price=buyprice)
+except NameError:
+    pass
 
-
-def my_function():
+#Sell orders
+try:
+    sellOrder = client.create_order(
+        symbol=sellticker,
+        side=SIDE_SELL,
+        type=newsellordertype,
+        timeInForce=TIME_IN_FORCE_GTC,
+        quantity=sellamount,
+        price=sellprice)
+except NameError:
+    pass
+    
+def loopreadfunc():
     while True:     # The Event Loop
         event, value = window.read()
-        if event in (None, 'EXIT'):            # quit if exit button or X
+        if event in (None):            # quit if exit button or X
             break
         if event == 'buy.':
-        
-            print(f'Buying {buyamount} {buyticker} at {buyprice}')
-        if event == 'sell':
-            return limitbuyOrder,
+            return buyOrder
+        if event == 'sell.':
+            return sellOrder
+
+def sendingorder():
+    if event == 'buy.':
         print(f'Buying {buyamount} {buyticker} at {buyprice}')
-
-
-
+    if event == 'sell.':
+        print(f'Selling {sellamount} {sellticker} at {sellprice}')
 
 
 ##############################################
